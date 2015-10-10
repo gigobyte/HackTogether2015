60ace1c30948b6f24e089400da05d05106c39e48
NoHomey <ivo_stratev@abv.bg>
2015-10-10 14:46:21 +0300

    Update and rename local_time.py to world_time.py

--- ivo/world_time.py
+++ ivo/world_time.py
@@ -6,31 +6,32 @@ import requests
 api_keys = {'google' : 'AIzaSyC6CsQc6B6nBvURAuqc53dLy8YVpgdXkOg' , 'timezonedb' : '7B2YJOH7HNP4'}
 
 gmaps = googlemaps.Client(key = api_keys['google'])
-fmt = '%Y.%m.%d %H : %M : %S'
 
-def GetGeo():
-    geo = gmaps.geocode('London')
-    res = geo[0]
-    return res
-
-def GetLocation():
-    res = GetGeo()
-    return res['geometry']['location']
-
-def req():
-    geo = GetLocation()
+def req(city):
+    geo = gmaps.geocode(city)
+    geo = geo[0]['geometry']['location']
     url = 'http://api.timezonedb.com/'
     query = {'key' : api_keys['timezonedb'], 'lat' : geo['lat'], 'lng' : geo['lng'], 'format' : 'json'}
     resp = requests.get(url, params = query)
     return resp.json()
 
-def GetTime():
-    local = req()
+def GetTime(city):
+    local = req(city)
     time = local['timestamp'] + int(local['gmtOffset'])
     zone = datetime.utcfromtimestamp(time)
     now = datetime.now()
     dif = now - zone
-    local = now + dif
-    return local.strftime(fmt)
+    return dif
 
-print GetTime()
+def Clock(city):
+    dif = GetTime(city)
+    fmt = '%Y.%m.%d %H : %M : %S'
+    second = 0
+    while 1:
+        local = dif + datetime.utcnow()
+        l_sec = local.second
+        if l_sec > second or l_sec == 0 and second == 59:
+            second = l_sec
+            print local.strftime(fmt)
+
+Clock('London')
