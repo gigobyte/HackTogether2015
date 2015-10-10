22350ad69760575f89e26eda0ebbdaf8cf391ce9
dragonkoko <dragonkoko@abv.bg>
2015-10-10 13:50:07 +0300

    show hotels,bars, e.t.c near you

---
+++ get_location.py
@@ -0,0 +1,15 @@
+import subprocess
+from pygeocoder import Geocoder
+from dictionary import (adb_commands, commands)
+from adb import (run_command)
+
+def loc():
+	a = run_command(adb_commands['get-location'])
+	a = a.split("Location[network")[1].split("acc")[0]
+	latitude = a.split(",")[0]
+	longitude = a.split(",")[1]
+	#print "latitude:", latitude
+	#print "longitude:", longitude
+
+	results = Geocoder.reverse_geocode(float(latitude),float(longitude))
+	return results.city
--- maps.py
+++ maps.py
@@ -1,45 +1,28 @@
 import webbrowser
 import subprocess
+from get_location import loc
 from pygeocoder import Geocoder
 from dictionary import (adb_commands, commands)
 from adb import (run_command)
 
-# def get_location():
-# 	google_maps = GoogleMaps(api_key = 'AIzaSyC6CsQc6B6nBvURAuqc53dLy8YVpgdXkOg')
-# 	address = ""
-# 	location = google_maps.search(location=address)
-# 	my_location = location.first()
-# 	print my_location
-# 	print(my_location.route)
-# 	print(my_location.street_number)
-# 	print(my_location.postal_code)
 
-location = 'varna'
+location = loc()
 
 
-
-def show_hotels(loc):
+def show_hotels():
 	hotels = webbrowser.open('https://www.google.bg/maps/search/hotel+' + location, new = 1,autoraise = True)
 	return hotels
 
-def show_restaurants(loc):
+def show_restaurants():
 	rastaurants = webbrowser.open('https://www.google.bg/maps/search/restaurant+' + location, new = 1,autoraise = True)
 	return restaurants
 
-def show_bars(loc):
+def show_bars():
 	bars = webbrowser.open('https://www.google.bg/maps/search/bar+' + location, new = 1,autoraise = True)
 	return bars
 
-def show_coffees(loc):
+def show_coffees():
 	coffees = webbrowser.open('https://www.google.bg/maps/search/coffee+' + location, new = 1,autoraise = True)
 	return coffees
 
-a = run_command(adb_commands['get-location'])
-a = a.split("Location[network")[1].split("acc")[0]
-latitude = a.split(",")[0]
-longitude = a.split(",")[1]
-print "latitude:", latitude
-print "longitude:", longitude
-
-results = Geocoder.reverse_geocode(float(latitude),float(longitude))
-print results.city
+#show_hotels()
