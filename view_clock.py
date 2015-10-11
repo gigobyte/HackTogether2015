3dda57e6006827d4a82d15ff4c45a381f1d8c3d7
dragonkoko <dragonkoko@abv.bg>
2015-10-11 13:39:05 +0300

    Commiting Clock!

---
+++ view_clock.py
@@ -0,0 +1,35 @@
+import Tkinter as tk
+from datetime import *
+from world_time import req, GetTime, Clock
+
+global second
+second = 0
+global t
+t = GetTime('London')
+print t
+
+def update_timeText():
+    # Get the current time, note you can change the format as you wish
+    	global second
+	global t
+	local = datetime.now()
+	l_sec = local.second
+	if l_sec > second or l_sec == 0 and second == 59:
+		second = l_sec
+		t = t + timedelta(seconds = 1)
+	current = t.strftime("%H:%M:%S")
+    # Update the timeText Label box with the current time
+	timeText.configure(text=current)
+    # Call the update_timeText() function after 1 second
+	root.after(1000, update_timeText)
+
+root = tk.Tk()
+root.overrideredirect(1)
+root.configure(background='white')
+root.wm_title("World Clock")
+
+# Create a timeText Label (a text box)
+timeText = tk.Label(root, text="", font=("Helvetica", 20))
+timeText.pack()
+update_timeText()
+root.mainloop()
---
+++ world_time.py
@@ -0,0 +1,37 @@
+import time
+from datetime import *
+import googlemaps
+import requests
+
+api_keys = {'google' : 'AIzaSyC6CsQc6B6nBvURAuqc53dLy8YVpgdXkOg' , 'timezonedb' : '7B2YJOH7HNP4'}
+
+gmaps = googlemaps.Client(key = api_keys['google'])
+
+def req(city):
+    geo = gmaps.geocode(city)
+    geo = geo[0]['geometry']['location']
+    url = 'http://api.timezonedb.com/'
+    query = {'key' : api_keys['timezonedb'], 'lat' : geo['lat'], 'lng' : geo['lng'], 'format' : 'json'}
+    resp = requests.get(url, params = query)
+    return resp.json()
+
+def GetTime(city):
+    local = req(city)
+    time = local['timestamp']
+    zone = datetime.utcfromtimestamp(time)
+    return zone
+
+def Clock(city):
+    t = GetTime(city)
+    return t
+    fmt = '%Y.%m.%d %H : %M : %S'
+    second = 0
+    while 1:
+        local = datetime.now()
+        l_sec = local.second
+        if l_sec > second or l_sec == 0 and second == 59:
+            second = l_sec
+            t = t + timedelta(seconds = 1)
+            print t.strftime(fmt)
+
+Clock('Harare')
