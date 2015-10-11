a5e1870cd5047c38d699a0f4dcaccf5651e7d426
martog <martin.holy.grigorov@gmail.com>
2015-10-11 14:27:53 +0300

    Perfect Cock :)

--- view_clock.py
+++ view_clock.py
@@ -1,13 +1,18 @@
-import Tkinter as tk
+import Tkinter
 from datetime import *
 from world_time import req, GetTime, Clock
+from PIL import Image, ImageTk
 import sys
 
 global second
 second = 0
 global t
-t = GetTime(sys.argv[1])
+t = GetTime('London')
 print t
+
+def onClosing():
+	global root
+	root.destroy()
 
 def update_timeText():
     # Get the current time, note you can change the format as you wish
@@ -24,14 +29,20 @@ def update_timeText():
     # Call the update_timeText() function after 1 second
 	root.after(1000, update_timeText)
 
-root = tk.Tk()
+global root
+root = Tkinter.Tk()
 root.overrideredirect(1)
 root.configure(background='white')
 root.wm_title("World Clock")
-root.geometry("110x50+1120+300")
+root.geometry("170x46+1195+360")
+
+''' SHOW EXIT ICON '''
+exit_image = ImageTk.PhotoImage(file="images/exit.png")
+exit_button = Tkinter.Button(root, image =exit_image, command = onClosing, background = "white", bd = 0)
+exit_button.pack(anchor = "se")
 
 # Create a timeText Label (a text box)
-timeText = tk.Label(root, text="", font=("Helvetica", 20))
+timeText = Tkinter.Label(root, text="", font=("Helvetica", 20))
 timeText.pack()
 update_timeText()
 root.mainloop()
