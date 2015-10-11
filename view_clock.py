a14c8b9792e6c920f41275bcc807eeab7a95ae9e
dragonkoko <dragonkoko@abv.bg>
2015-10-11 13:58:17 +0300

    Updating Clock

--- view_clock.py
+++ view_clock.py
@@ -19,7 +19,7 @@ def update_timeText():
 		t = t + timedelta(seconds = 1)
 	current = t.strftime("%H:%M:%S")
     # Update the timeText Label box with the current time
-	timeText.configure(text=current)
+	timeText.configure(text=current, background='white')
     # Call the update_timeText() function after 1 second
 	root.after(1000, update_timeText)
 
@@ -27,6 +27,7 @@ root = tk.Tk()
 root.overrideredirect(1)
 root.configure(background='white')
 root.wm_title("World Clock")
+root.geometry("110x50+1120+300")
 
 # Create a timeText Label (a text box)
 timeText = tk.Label(root, text="", font=("Helvetica", 20))
